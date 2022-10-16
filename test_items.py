2cfec7fbd3913870866e46485a9cf9d019e14cc6
Ali Sadretdinov <sadrali1990@gmail.com>
2022-10-16 19:29:54 +0300

    Chaged name of function in test_items.py

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ import time
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language_parametrs_via_cmd(browser):
+def test_add_to_cart_button_is_exist(browser):
     browser.get(link)
     time.sleep(30)
     assert browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
